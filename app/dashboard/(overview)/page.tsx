aa89c44cc0b96b54e6f9f272dc0f4b9ccb0ca34d
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-19 07:45:26 -0500

    Completed Chapter 16, metadata.
    Learned how what is metadata, what types of metadata exists, how to use Open Graph and used metadata API to add titles.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,11 @@ import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import { lusitana } from "../../ui/fonts";
 import { Suspense } from "react";
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '../../ui/skeletons';
-
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 export default async function Page() {
     return (
         <main>
